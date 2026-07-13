b21dabf746366e62473a38450de18c2b47dc942e
peregreen1 <misterfung@gmail.com>
2026-07-12 20:04:17 -0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,15 @@
+"""
+Custom exceptions used throughout the WindfallLab engine.
+"""
 
+
+class WindfallError(Exception):
+    """Base class for all WindfallLab exceptions."""
+
+
+class InsufficientCashError(WindfallError):
+    """Raised when attempting to invest more cash than is available."""
+
+
+class InvalidTransactionError(WindfallError):
+    """Raised when a transaction contains invalid data."""
